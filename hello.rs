120682e17f7581c5815f1c55900d0b738d94fc4b
redmonroe <joewalsh@vistulaofindiana.com>
2023-01-15 22:00:52 -0500

    first

--- hello.rs
+++ hello.rs
@@ -2,4 +2,5 @@ fn main() {
     // the main function is special: it is always the first code that runs in every executable Rust program
     // Rust requires curly brackets around all function bodies
     println!("Hello, World!");
+    println!("Hello, World2.!");
 }
