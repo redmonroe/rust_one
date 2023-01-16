1941f8e8af60608e158764f210967ddf9607957a
redmonroe <joewalsh@vistulaofindiana.com>
2023-01-15 21:42:48 -0500

    first rust commit

---
+++ hello.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // the main function is special: it is always the first code that runs in every executable Rust program
+    // Rust requires curly brackets around all function bodies
+    println!("Hello, World!");
+}
